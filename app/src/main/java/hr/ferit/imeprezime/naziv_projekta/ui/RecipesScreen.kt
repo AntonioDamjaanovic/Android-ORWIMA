32b02d0faa9f9721228a67637cbe320b4379b961
Antonio Damjanovic <damjanovic1109@gmail.com>
2024-12-02 22:40:19 +0100

    Add LV2

--- app/src/main/java/hr/ferit/imeprezime/naziv_projekta/ui/RecipesScreen.kt
+++ app/src/main/java/hr/ferit/imeprezime/naziv_projekta/ui/RecipesScreen.kt
@@ -1,6 +1,7 @@
-package hr.ferit.imeprezime.naziv_projekta.ui.theme
+package hr.ferit.imeprezime.naziv_projekta.ui
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -23,18 +24,13 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.DarkGray
-import androidx.compose.ui.graphics.Color.Companion.LightGray
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
@@ -42,6 +38,14 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import hr.ferit.imeprezime.naziv_projekta.ui.theme.DarkGray
+import hr.ferit.imeprezime.naziv_projekta.ui.theme.LightGray
+import hr.ferit.imeprezime.naziv_projekta.ui.theme.Pink
+import hr.ferit.imeprezime.naziv_projekta.ui.theme.Purple500
+import hr.ferit.imeprezime.naziv_projekta.ui.theme.White
+import hr.ferit.imeprezime.naziv_projekta.R
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Preview(showBackground = true)
 @Composable
@@ -55,9 +59,35 @@ fun RecipesScreen() {
             title="What would you like to cook today?",
             subtitle="Good morning, Antonio"
         )
+        SearchBar(
+            iconResource = R.drawable.ic_search,
+            labelText = "Search..."
+        )
+        RecipeCategories()
+        RecipeCount(
+            iconResource =  R.drawable.ic_flame,
+            labelText =  "7 recipes"
+        )
+        Row()
+        {
+            RecipeCard(
+                iconResource = R.drawable.strawberry_pie_1,
+                title = "Strawberry pie"
+            )
+            Spacer(Modifier.width(4.dp))
+            RecipeCard(
+                iconResource = R.drawable.strawberry_pie_2,
+                title = "Strawberry pie 2"
+            )
+        }
+        IconButton(
+            R.drawable.ic_plus,
+            "Add new recipe"
+        )
 
     }
 }
+
 
 @Composable
 fun ScreenTitle(
@@ -66,25 +96,33 @@ fun ScreenTitle(
 ) {
     Box(
         modifier = Modifier
-            .padding(top = 16.dp)
             .fillMaxWidth()
+            .padding(top = 16.dp)
     ) {
         Text(
             text = subtitle,
-            style = TextStyle(color = Color.Magenta, fontSize = 12.sp,
-                fontWeight = FontWeight.Light, fontStyle = FontStyle.Italic),
-            modifier = Modifier
-                .padding(horizontal = 15.dp)
+            style = TextStyle(
+                color = Purple500,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Light,
+                fontStyle = FontStyle.Italic
+            ),
+            modifier = Modifier.padding(horizontal = 15.dp, vertical = 2.dp)
         )
-        Text(
-            text = title,
-            style = TextStyle(color = Color.Black, fontSize = 26.sp,
-                fontWeight = FontWeight.Bold),
-            modifier = Modifier
-                .padding(vertical = 16.dp, horizontal = 16.dp)
-        )
+
     }
+    Text(
+        text = title,
+        style = TextStyle(
+            color = Color.Black,
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold
+        ),
+        modifier = Modifier
+            .padding(vertical = 1.dp, horizontal = 16.dp)
+    )
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -93,8 +131,8 @@ fun SearchBar(
     labelText: String,
     colors: TextFieldColors = TextFieldDefaults.textFieldColors(
         containerColor = Color.Transparent,
-        //placeholderColor = DarkGray,
-        //textColor = DarkGray,
+        unfocusedPlaceholderColor = DarkGray,
+        focusedTextColor = DarkGray,
         unfocusedLabelColor = DarkGray,
         focusedIndicatorColor = Color.Transparent,
         unfocusedIndicatorColor = Color.Transparent,
@@ -136,10 +174,14 @@ fun TabButton(
     Button(
         shape = RoundedCornerShape(24.dp),
         elevation = null,
-        colors = if (isActive) ButtonDefaults.buttonColors(contentColor = White,
-            containerColor = Pink40) else
-            ButtonDefaults.buttonColors(contentColor = DarkGray,
-                containerColor = LightGray),
+        colors = if (isActive) ButtonDefaults.buttonColors(
+            contentColor = White,
+            containerColor = Pink
+        ) else
+            ButtonDefaults.buttonColors(
+                contentColor = DarkGray,
+                containerColor = LightGray
+            ),
         modifier = Modifier.fillMaxHeight(),
         onClick = { onClick() }
     ) {
@@ -158,14 +200,20 @@ fun RecipeCategories() {
             .fillMaxWidth()
             .height(44.dp)
     ) {
-        TabButton(text = "All", isActive = currentActiveButton == 0) {
+        TabButton(
+            text = "All",
+            isActive = currentActiveButton == 0) {
             currentActiveButton = 0
         }
-        TabButton(text = "Breakfast", isActive = currentActiveButton == 1
+        TabButton(
+            text = "Breakfast",
+            isActive = currentActiveButton == 1
         ){
             currentActiveButton = 1
         }
-        TabButton("Lunch", currentActiveButton == 2) {
+        TabButton(
+            "Lunch",
+            currentActiveButton == 2) {
             currentActiveButton = 2
         }
     }
@@ -177,21 +225,25 @@ fun IconButton(
     text: String
 ) {
     Button(
-        onClick = { /*TODO*/ },
-        colors = ButtonDefaults.buttonColors(containerColor = Pink40),
-    ) {
+        onClick = {  },
+        colors = ButtonDefaults.buttonColors(containerColor = Pink),
+
+        ) {
         Row {
             Icon(
                 painter = painterResource(id = iconResource),
                 contentDescription = text
             )
+
             Spacer(Modifier.width(2.dp))
+
             Text(
                 text = text,
                 style = TextStyle(
                     fontSize = 16.sp,
                     fontWeight = FontWeight.Light
-                )
+                ),
+                modifier = Modifier.padding(vertical = 2.dp)
             )
         }
     }
@@ -201,7 +253,7 @@ fun IconButton(
 fun Chip(
     text: String,
     backgroundColor: Color = Color.White,
-    textColor: Color = Color.Magenta,
+    textColor: Color = Pink,
 ) {
     Box(
         modifier = Modifier
@@ -214,10 +266,85 @@ fun Chip(
     ) {
         Text(
             text = text,
-            style = TextStyle(
-                color = textColor,
-                fontSize = 12.sp
+            color = textColor,
+            fontSize = 12.sp
+        )
+    }
+}
+
+@Composable
+fun RecipeCard(
+    @DrawableRes iconResource: Int,
+    title: String
+) {
+    Box(
+        modifier = Modifier
+            .width(215.dp)
+            .height(326.dp)
+    )
+    {
+
+        Image(
+            painter = painterResource(id = iconResource),
+            contentDescription = title,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(RoundedCornerShape(24.dp))
+        )
+
+        Column (
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 5.dp)
+
+        ) {
+            Text(
+                text = title,
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+
+                modifier = Modifier
+                    .padding(vertical = 2.dp, horizontal = 16.dp)
+
             )
+            Row (
+                modifier = Modifier
+                    .padding(vertical = 12.dp, horizontal = 16.dp)
+            ) {
+                Chip("30 mins")
+                Spacer(Modifier.width(2.dp))
+                Chip("4 Ingredients")
+            }
+        }
+    }
+}
+
+@Composable
+fun RecipeCount(
+    @DrawableRes iconResource: Int,
+    labelText: String,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+    )
+    {
+        Text(
+            text = labelText,
+            modifier = Modifier.padding(vertical = 8.dp)
+        )
+        Icon(
+            painter = painterResource(id = iconResource),
+            contentDescription = "text",
+            modifier = Modifier
+                .width(12.dp)
+                .height(12.dp)
         )
     }
 }
--- app/src/main/java/hr/ferit/imeprezime/naziv_projekta/ui/theme/Theme.kt
+++ app/src/main/java/hr/ferit/imeprezime/naziv_projekta/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package hr.ferit.imeprezime.naziv_projekta.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,15 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Purple200,
+    secondary = Gray,
+    tertiary = Pink
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Purple200,
+    secondary = Gray,
+    tertiary = Pink
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun MyApplicationTheme(
+fun RecipeCardTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
